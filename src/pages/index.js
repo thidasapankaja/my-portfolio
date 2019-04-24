228d020d3902f506add9881846bbbf81b1fd3ea7
pankaja92 <pankajanc@gmail.com>
2019-04-24 08:08:48 +0530

    Removed Lazy loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React, { lazy, Suspense} from "react"
 import Layout from "../components/layout"
+import About from '../components/about';
 
 import SEO from '../components/seo'
 
 import '../components/layout.css'
 
-const About = lazy(() => import('../components/about'));
+// const About = lazy(() => import('../components/about'));
 
 export default () => (
   <Layout>
     <SEO title="About me" description="Brief description about who am I"/>
-    <Suspense fallback={<span>Loading ...</span>}>
+    {/* <Suspense fallback={<span>Loading ...</span>}>
       <About />
-    </Suspense>
+    </Suspense> */}
+     <About />
   </Layout>
 )
