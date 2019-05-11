1e9de390cbe227ec86577c2046cf2c2b15c4c5bd
pankaja92 <pankajanc@gmail.com>
2019-05-11 07:43:41 +0530

    Google analyitcs fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
