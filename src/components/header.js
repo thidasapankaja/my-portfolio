0b2c2f7a5269cfc6b46bd900442b0acb43160eb4
pankaja92 <pankajanc@gmail.com>
2019-07-27 09:51:22 +0530

    Styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,31 @@ import React from "react";
 import { Link } from "gatsby";
 import me from '../assets/me.png';
 
+import styled from 'styled-components';
+
+const Li = styled.li`
+  display: inline-block;
+  margin-left: 1rem;
+
+  @media (max-width: 380px) {
+    display: inline-block;
+    margin-left: 0.5rem;
+  }
+`
+
+const HeaderItem = styled.header`
+  display:flex;
+  justify-content:space-between;
+`
+
 const ListLink = props => (
-  <li className='header-li' key={props.to}>
+  <Li key={props.to}>
     <Link className='linkStyle' to={props.to} activeClassName='activeClass'>{props.children}</Link>
-  </li>
+  </Li>
 )
 
 const Header = () => (
-    <header className='header'>
+    <HeaderItem>
       <Link to="/">
         <h3><img src={me} style={{height:'40px', width:'40px'}} alt='me'/></h3>
       </Link>
@@ -19,7 +36,7 @@ const Header = () => (
         <ListLink to="/contact"><h3>Contact</h3></ListLink>
         <ListLink to="/blog"><h3>Blog</h3></ListLink>
       </ul>
-    </header>
+    </HeaderItem>
 )
 
 export default Header;
