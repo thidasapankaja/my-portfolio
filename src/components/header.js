c4f3a60acf7047104e7f2ef4d8aa53509d2e714d
pankaja92 <pankajanc@gmail.com>
2019-07-27 09:41:00 +0530

    Fixed header styling issue

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,14 @@ import React from "react";
 import { Link } from "gatsby";
 import me from '../assets/me.png';
 
-import styled from 'styled-components';
-
-const Li = styled.li`
-  display: inline-block;
-  margin-left: 1rem;
-
-  @media (max-width: 380px) {
-    display: inline-block;
-    margin-left: 0.5rem;
-  }
-`
-
-const HeaderItem = styled.header`
-  display:flex;
-  justify-content:space-between;
-`
-
 const ListLink = props => (
-  <Li key={props.to}>
+  <li className='header-li' key={props.to}>
     <Link className='linkStyle' to={props.to} activeClassName='activeClass'>{props.children}</Link>
-  </Li>
+  </li>
 )
 
 const Header = () => (
-    <HeaderItem>
+    <header className='header'>
       <Link to="/">
         <h3><img src={me} style={{height:'40px', width:'40px'}} alt='me'/></h3>
       </Link>
@@ -36,7 +19,7 @@ const Header = () => (
         <ListLink to="/contact"><h3>Contact</h3></ListLink>
         <ListLink to="/blog"><h3>Blog</h3></ListLink>
       </ul>
-    </HeaderItem>
+    </header>
 )
 
 export default Header;
